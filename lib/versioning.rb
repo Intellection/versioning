efb6c1d2b344f3c2dade4397bd0dfc0500ac7f9e
Rudy Seidinger <rudyseidinger@gmail.com>
2016-05-10 13:00:44 +0100

    Require version file

--- lib/versioning.rb
+++ lib/versioning.rb
@@ -4,6 +4,7 @@ module Versioning
 end
 
 require_relative "versioning/git"
+require_relative "versioning/version_tag"
 require_relative "versioning/version"
 require_relative "versioning/version_file"
 require_relative "versioning/railtie" if defined?(Rails)
